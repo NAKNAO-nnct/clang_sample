1f7ed5f99ae34024f1cf2fb7b0ec947c0a34c2b3
NAKNAO-nnct <totoro.windows@outlook.com>
2019-10-17 23:55:42 +0900

    インデント修正

--- fcomp.c
+++ fcomp.c
@@ -1,21 +1,24 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-typedef struct {	/* 分数の構造体 */
-    int numer;		/* 分子 */
-    int denom;		/* 分母 */
+typedef struct
+{              /* 分数の構造体 */
+    int numer; /* 分子 */
+    int denom; /* 分母 */
 } FRACTION;
 
-typedef struct {	/* 複素数の構造体 */
-    FRACTION real;	/* 実部 */
-    FRACTION imag;	/* 虚部 */
+typedef struct
+{                  /* 複素数の構造体 */
+    FRACTION real; /* 実部 */
+    FRACTION imag; /* 虚部 */
 } FCOMP;
 
 /* 関数を追加する場合はここに */
 // sub-1
-FCOMP sub(FCOMP a){
+FCOMP sub(FCOMP a)
+{
     FCOMP result;
-    result.real.numer = a.real.numer-a.real.denom;
+    result.real.numer = a.real.numer - a.real.denom;
     result.real.denom = a.real.denom;
     result.imag.numer = a.imag.numer;
     result.imag.denom = a.imag.denom;
@@ -23,34 +26,36 @@ FCOMP sub(FCOMP a){
 }
 
 // multiplication
-FCOMP mult(FCOMP a, FCOMP b){
+FCOMP mult(FCOMP a, FCOMP b)
+{
     FCOMP result;
-    int X = a.real.denom*a.imag.denom*b.real.denom*b.imag.denom;
-    int bc = a.real.numer*a.imag.denom;
-    int eh = b.real.denom*b.imag.numer;
-    int fg = b.real.numer*b.imag.denom;
-    int ad = a.real.denom*a.imag.numer;
-    result.real.numer = bc*fg + ad*eh;
+    int X = a.real.denom * a.imag.denom * b.real.denom * b.imag.denom;
+    int bc = a.real.numer * a.imag.denom;
+    int eh = b.real.denom * b.imag.numer;
+    int fg = b.real.numer * b.imag.denom;
+    int ad = a.real.denom * a.imag.numer;
+    result.real.numer = bc * fg + ad * eh;
     result.real.denom = X;
-    result.imag.numer = bc*eh+ad*fg;
+    result.imag.numer = bc * eh + ad * fg;
     result.imag.denom = X;
     return result;
 }
 
 // division
-FCOMP divi(FCOMP a, FCOMP b){
+FCOMP divi(FCOMP a, FCOMP b)
+{
     FCOMP result;
-    int BD = a.real.denom*a.imag.denom;
-    result.real.numer = b.real.numer*BD;
-    int X = a.real.numer*a.imag.denom - a.real.denom*a.imag.numer;
-    result.real.denom = X*b.real.denom;
-    result.imag.numer = b.imag.numer*BD;
-    result.imag.denom = X*b.real.denom;
-    printf("%d\n",b.real.denom);
+    int BD = a.real.denom * a.imag.denom;
+    result.real.numer = b.real.numer * BD;
+    int X = a.real.numer * a.imag.denom - a.real.denom * a.imag.numer;
+    result.real.denom = X * b.real.denom;
+    result.imag.numer = b.imag.numer * BD;
+    result.imag.denom = X * b.real.denom;
+    printf("%d\n", b.imag.numer);
     return result;
 }
 
-FCOMP fcomp_comp( FCOMP x )
+FCOMP fcomp_comp(FCOMP x)
 {
     /* ここを作る  */
     FCOMP temp_A1, temp_A2, temp_A3, temp_A4;
@@ -62,15 +67,15 @@ FCOMP fcomp_comp( FCOMP x )
     return temp_A4;
 }
 
-int main( int argc, char **argv )
+int main(int argc, char **argv)
 {
-    FCOMP a,b;
+    FCOMP a, b;
 
     a.real.numer = atoi(argv[1]);
     a.real.denom = atoi(argv[2]);
     a.imag.numer = atoi(argv[3]);
     a.imag.denom = atoi(argv[4]);
 
-    b = fcomp_comp( a );
-    printf( "(%d/%d)+j(%d/%d)\n",b.real.numer,b.real.denom, b.imag.numer,b.imag.denom );
+    b = fcomp_comp(a);
+    printf("(%d/%d)+j(%d/%d)\n", b.real.numer, b.real.denom, b.imag.numer, b.imag.denom);
 }
